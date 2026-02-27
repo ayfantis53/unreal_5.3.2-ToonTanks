2d2f001e5b2bc359299fb124a08ffd99bde1f650
ayfantis53 <85624117+ayfantis53@users.noreply.github.com>
2026-02-27 13:49:41 -0700

    Update ToonTanks.cpp

--- Source/ToonTanks/ToonTanks.cpp
+++ Source/ToonTanks/ToonTanks.cpp
@@ -1,15 +1,7 @@
-// ============================================================================== //
-//							Unreal Engine version 5.3.2	
-// ============================================================================== //
-// Toon Tanks	  
-// ____________________________	
-// Developed by Andrew Yfantis.
-// https://github.com/ayfantis53
-//
-// 2025
-// ============================================================================== //
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "ToonTanks.h"
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ToonTanks, "ToonTanks" );
+
